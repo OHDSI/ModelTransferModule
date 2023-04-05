ee44b95c7cd74b56852f8cce722e73dced815373
jreps <jreps@its.jnj.com>
2023-04-05 12:09:35 -0400

    update renv activate
    - update renv activate

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -6,7 +6,7 @@ createModelTransferModuleSpecifications <- function(
   
   specifications <- list(
     module = "ModelTransferModule",
-    version = "0.0.2",
+    version = "0.0.3",
     remoteRepo = "github.com",
     remoteUsername = "ohdsi",
     settings = list(
