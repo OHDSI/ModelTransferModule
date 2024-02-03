cfc0318820cfb472d11c4c24ecfc0b3bb614278d
Henrik John <l.john@erasmusmc.nl>
2024-02-03 10:02:54 +0100

    Transfer Strategus outputs

--- Main.R
+++ Main.R
@@ -25,7 +25,7 @@ getModuleInfo <- function() {
 execute <- function(jobContext) {
   rlang::inform("Validating inputs")
   inherits(jobContext, 'list')
-
+  
   if (is.null(jobContext$settings)) {
     stop("Analysis settings not found in job context")
   }
@@ -81,8 +81,8 @@ execute <- function(jobContext) {
 }
 
 getModelsFromLocalFiles <- function(
-  localFileSettings,
-  saveFolder
+    localFileSettings,
+    saveFolder
 ){
   
   if(is.null(localFileSettings)){
@@ -116,7 +116,7 @@ getModelsFromLocalFiles <- function(
       modelSaved <- T
       
     }
-
+    
     info <- rbind(
       info,
       data.frame(
@@ -132,8 +132,8 @@ getModelsFromLocalFiles <- function(
 
 # code that takes s3 details and download the models and returns the locations plus details as data.frame
 getModelsFromS3 <- function(
-  s3Settings,
-  saveFolder
+    s3Settings,
+    saveFolder
 ){
   
   # need to have settings
@@ -158,35 +158,38 @@ getModelsFromS3 <- function(
     )
     
     if(validBucket){
+      subfolder <- s3Settings$modelZipLocation[i]
+      bucket <- s3Settings$bucket[i]
+      region <- s3Settings$region[i]
       
-      modelExists <- aws.s3::object_exists(
-        object = s3Settings$modelZipLocation[i],
-        bucket = s3Settings$bucket[i], 
-        region = s3Settings$region[i]
-      )
+      result <- aws.s3::get_bucket_df(bucket = bucket, region = region)
+      paths <- fs::path(result$Key)
       
-      if(modelExists){
+      workDir <- findWorkDir(bucket, subfolder, region)
+      analyses <- findAnalysesNames(bucket, workDir, region)
+      
+      if(length(analyses) > 0) {
         if(!dir.exists(file.path(saveFolder, "models"))){
           dir.create(file.path(saveFolder, "models"), recursive = T)
         }
-        saveToLoc <- file.path(saveFolder, "models", paste0("model_s3_",i))
-        tempDir <- file.path(tempdir(), paste0('model',i,'.zip'))
+        saveToLoc <- file.path(saveFolder, "models")
         
-        # move the model to a local file
-        aws.s3::save_object(
-          object = s3Settings$modelZipLocation[i],
-          bucket = s3Settings$bucket[i], 
-          region = s3Settings$region[i],
-          file = tempDir
-        )
-        modelSaved <- T
-        
-        # unzip into the workFolder
-        OhdsiSharing::decompressFolder(
-          sourceFileName = tempDir, 
-          targetFolder = saveToLoc 
-          )
-        
+        for (analysis in analyses) {
+          analysis_paths <- paths[fs::path_has_parent(paths, fs::path(workDir, analysis))]
+          
+          for(obj in analysis_paths) {
+            # split work directory from path
+            relative_paths <- fs::path_rel(obj, start = workDir)
+            # remove artifacts created by current path location
+            filtered_paths <- relative_paths[relative_paths != "."]
+            # Construct the file path where you want to save the file locally
+            local_file_path <- fs::path(saveToLoc, filtered_paths)
+            
+            # Download the file from S3
+            aws.s3::save_object(obj, bucket, file = local_file_path)
+            ParallelLogger::logInfo(paste0("Downloaded: ", obj, " to ", local_file_path, "\n"))
+          }
+        }
       } else{
         ParallelLogger::logInfo(paste0("No ",s3Settings$modelZipLocation[i]," in bucket ", s3Settings$bucket[i], " in region ", s3Settings$region[i] ))
       } 
@@ -197,8 +200,8 @@ getModelsFromS3 <- function(
     info <- rbind(
       info,
       data.frame(
-        originalLocation = s3Settings$modelZipLocation[i], 
-        modelSavedLocally = modelSaved, 
+        originalLocation = "PLACEHOLDER", 
+        modelSavedLocally = TRUE, 
         localLocation = saveToLoc
       )
     )
@@ -210,8 +213,8 @@ getModelsFromS3 <- function(
 
 # code that takes github details and download the models and returns the locations plus details as data.frame
 getModelsFromGithub <- function(
-  githubSettings,
-  saveFolder
+    githubSettings,
+    saveFolder
 ){
   
   if(is.null(githubSettings)){
@@ -225,7 +228,7 @@ getModelsFromGithub <- function(
     githubUser <- githubSettings[[i]]$githubUser #'ohdsi-studies'
     githubRepository <- githubSettings[[i]]$githubRepository #'lungCancerPrognostic'
     githubBranch <- githubSettings[[i]]$githubBranch #'master'
-
+    
     downloadCheck <- tryCatch({
       utils::download.file(
         url = file.path("https://github.com",githubUser,githubRepository, "archive", paste0(githubBranch,".zip")),
@@ -283,9 +286,43 @@ getModelsFromGithub <- function(
         )
       )
     }
-      
+    
     
   }
-
+  
   return(info)
+}
+
+findWorkDir <- function(bucket, subfolder, region) {
+  # list all content in the bucket
+  result <- aws.s3::get_bucket_df(bucket = bucket, region = region)
+  # extract paths of all content
+  paths <- fs::path(result$Key)
+  # split paths up for easier processing
+  split_path <- fs::path_split(paths)
+  
+  # find the full path of the subfolder with models for validation
+  results_sapply <- sapply(split_path, function(x) { 
+    identical(tail(x, 1), subfolder) 
+  })
+  subfolder_path <- paths[results_sapply]
+  
+  return(subfolder_path)
+}
+
+findAnalysesNames <- function(bucket, workDir, region) {
+  # list all content in the bucket
+  result <- aws.s3::get_bucket_df(bucket = bucket, region = region)
+  # extract paths of all content
+  paths <- fs::path(result$Key)
+  # filter for paths in work directory
+  work_dir_paths <- paths[fs::path_has_parent(paths, workDir)]
+  # split work directory from path
+  relative_paths <- fs::path_rel(work_dir_paths, start = workDir)
+  # remove artifacts created by current path location
+  filtered_paths <- relative_paths[relative_paths != "."]
+  # get only the top level directories
+  top_level_dirs <- sapply(fs::path_split(filtered_paths), function(p) p[[1]])
+  top_level_dirs <- unique(top_level_dirs)
+  return(top_level_dirs)
 }
